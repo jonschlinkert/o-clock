cd19e7014ec2a99c3973805780f80c9c3670e78a
jonschlinkert <dev@sellside.com>
2014-12-07 03:08:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+/*!
+ * twelve-hours <https://github.com/jonschlinkert/twelve-hours>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function oclock(date) {
+  date = date instanceof Date
+    ? date : new Date();
+  return hours(date);
+};
+
+function hours(date) {
+  var hrs = date.getHours();
+  var min = minutes(date);
+  return hrs >= 12
+    ? hrs - 12 + min + ' PM'
+    : hrs + min + ' AM';
+}
+
+function minutes(date) {
+  var min = date.getMinutes();
+  var res = min < 10
+    ? '0' + min
+    : min;
+  return ':'+ res;
+}
---
+++ test.js
@@ -0,0 +1,16 @@
+/**
+ * o-clock <https://github.com/jonschlinkert/o-clock>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+var should = require('should');
+var moment = require('moment');
+var oclock = require('./');
+
+describe('oclock()', function () {
+  it('should return current time.', function () {
+    oclock().should.equal(moment().format('h:mm A'));
+  });
+});
